7ebeb65b375642be7f0c88cda6dd0a6109ce85b1
haoyue <1721684479@qq.com>
2021-10-19 23:19:14 +0800

    优化代码，空间实在是不知道怎么压缩了，还是太菜了

--- 1.twoSum.cpp
+++ 1.twoSum.cpp
@@ -11,31 +11,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        vector<int> res;
-        map<int, vector<int> > mMap;
+        int subResult;
+        map<int, int> mMap;
         for(int i = 0; i < nums.size(); ++i)
         {
-            if(mMap.find(nums[i]) == mMap.end())
-                mMap.insert(pair<int, vector<int> >(nums[i], { i }));
-            else
-                mMap[nums[i]].push_back(i);
-        }
-        for(int i = 0; i < nums.size(); ++i)
-        {
-            int subResult = target - nums[i];
+            subResult = target - nums[i];
             if(mMap.find(subResult) != mMap.end())
             {
-                if(mMap[subResult].size() == 1 and mMap[subResult][0] == i)
-                    continue;
-                res.push_back(i);
-                if(subResult == nums[i])
-                    res.push_back(mMap[subResult][1]);
-                else
-                    res.push_back(mMap[subResult][0]);
-                break;
+                return {i, mMap[subResult]};
             }
+            mMap[nums[i]]= i;
         }
-        return res;
+        return {};
     }
 };
 
