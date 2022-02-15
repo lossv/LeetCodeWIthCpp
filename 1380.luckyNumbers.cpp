d953259877be18f1077ac82c8d15302932d9daae
lossv <1721684479@qq.com>
2022-02-15 20:55:39 +0800

    https://leetcode-cn.com/problems/lucky-numbers-in-a-matrix/

---
+++ 1380.luckyNumbers.cpp
@@ -0,0 +1,42 @@
+//
+// Created by lossv on 2022/2/15.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> luckyNumbers(vector<vector<int>> &matrix)
+    {
+        vector<int> ans;
+        int n = matrix.size();
+        for (int i = 0; i < n; ++i)
+        {
+            auto minIndex = min_element(matrix[i].begin(), matrix[i].end());
+            bool flag = true;
+            for (int j = 0; j < n; ++j)
+            {
+                if (matrix[j][minIndex - matrix[i].begin()] > (*minIndex))
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                ans.emplace_back(*minIndex);
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
