8a5a55962ff1a72812cd74e0aa5d9b1fe1b00b5d
lossv <1721684479@qq.com>
2022-03-05 18:09:36 +0800

    https://leetcode-cn.com/problems/longest-uncommon-subsequence-i/

---
+++ 969.pancakeSort.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 觉而不绝 on 2022/2/19.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> pancakeSort(vector<int> &arr)
+    {
+        vector<int> ans;
+        for (int i = 0; i < arr.size(); ++i)
+        {
+            auto m = max_element(arr.begin(), arr.end() - i);
+            if (m == arr.end() - i - 1) continue;
+            reverse(arr.begin(), m + 1);
+            ans.push_back(m - arr.begin() + 1);
+            reverse(arr.begin(), arr.end() - i);
+            ans.push_back(arr.end() - i - arr.begin());
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
