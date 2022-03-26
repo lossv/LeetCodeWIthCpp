d4d2cddad2628c2cc928b44df8fa72bd38be8238
lossv <1721684479@qq.com>
2022-03-26 17:28:57 +0800

    https://leetcode-cn.com/problems/baseball-game/

---
+++ 682.calPoints.cpp
@@ -0,0 +1,52 @@
+//
+// Created by 觉而不绝 on 2022/3/26.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int calPoints(vector<string> &ops)
+    {
+        vector<int> nums;
+        for (auto &item: ops)
+        {
+            if (item[0] == '+')
+            {
+                nums.emplace_back(nums[nums.size() - 1] + nums[nums.size() - 2]);
+            }
+            else if (item[0] == 'D')
+            {
+                nums.emplace_back(nums[nums.size() - 1] * 2);
+            }
+            else if (item[0] == 'C')
+            {
+                nums.erase(nums.end() - 1);
+            }
+            else
+            {
+                nums.emplace_back(stoi(item));
+            }
+        }
+        int res = 0;
+        std::for_each(nums.begin(), nums.end(), [&](int n)
+        {
+            res += n;
+        });
+        return res;
+    }
+};
+
+int main()
+{
+    vector<string> list = {"5", "-2", "4", "C", "D", "9", "+", "+"};
+    Solution solution;
+    cout << solution.calPoints(list) << endl;
+    return 0;
+}
