305b9756817b5a76ff64075ef2bd8896f73b30f7
lossv <1721684479@qq.com>
2023-04-23 16:14:36 +0800

    https://leetcode.cn/problems/candy/?envType=study-plan-v2&id=top-interview-150

---
+++ 135.candy.cpp
@@ -0,0 +1,35 @@
+#include "comm.h"
+
+using namespace std;
+
+class Solution
+{
+public:
+    int candy(vector<int> &ratings)
+    {
+        int n = ratings.size();
+        int ret = 1;
+        int inc = 1, dec = 0, pre = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (ratings[i] >= ratings[i - 1])
+            {
+                dec = 0;
+                pre = ratings[i] == ratings[i - 1] ? 1 : pre + 1;
+                ret += pre;
+                inc = pre;
+            }
+            else
+            {
+                dec++;
+                if (dec == inc)
+                {
+                    dec++;
+                }
+                ret += dec;
+                pre = 1;
+            }
+        }
+        return ret;
+    }
+};
